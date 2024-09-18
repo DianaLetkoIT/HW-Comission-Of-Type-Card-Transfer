b947f43e80d88d242f8ba2265ab41f21e00dba41
Diana Letko <diana.letko@bk.ru>
2024-09-18 14:42:16 +0300

    Commit 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val typeOfCard = "Visa"
-    var previousTransfers = 0
-    var currentTransfers: Int = 150000
+    val typeOfCard = "Mastercard"
+    var previousTransfers = 500000
+    var currentTransfers: Int = 15000
     var answer = isComission(typeOfCard, previousTransfers, currentTransfers)
     println(answer)
 
@@ -10,15 +10,20 @@ fun main() {
 
 
 fun isComission(typeOfCard: String, previousTransfers: Int, currentTransfers: Int) =
-    if (previousTransfers <= 600000) {
+    if (previousTransfers + currentTransfers <= 600000) {
         if (currentTransfers <= 150000) {
             when {
                 typeOfCard == "Mastercard" -> {
-                    if (currentTransfers > 75000) {
-                        var comission = (currentTransfers - 75000) * 0.006 + 20
-                        "Комиссия равна $comission руб"
+                    if (previousTransfers < 75000) {
+                        if (currentTransfers + previousTransfers > 75000) {
+                            var comission = (currentTransfers - (75000 - previousTransfers)) * 0.006 + 20
+                            "Комиссия равна $comission руб"
+                        } else {
+                            "Комиссия равна 0 руб"
+                        }
                     } else {
-                        "Комиссия равна 0 руб"
+                        var comission = currentTransfers * 0.006 + 20
+                        "Комиссия рвна $comission руб"
                     }
                 }
 
@@ -41,9 +46,82 @@ fun isComission(typeOfCard: String, previousTransfers: Int, currentTransfers: In
         }
 
 
+    } else if (previousTransfers < 600000) {
+        if (currentTransfers <= (600000 - previousTransfers)) {
+            if (currentTransfers <= 150000) {
+                when {
+                    typeOfCard == "Mastercard" -> {
+                        if (previousTransfers < 75000) {
+                            if (currentTransfers + previousTransfers > 75000) {
+                                var comission = (currentTransfers - (75000 - previousTransfers)) * 0.006 + 20
+                                "Комиссия равна $comission руб"
+                            } else {
+                                "Комиссия равна 0 руб"
+                            }
+                        } else {
+                            var comission = currentTransfers * 0.006 + 20
+                            "Комиссия рвна $comission руб"
+                        }
+                    }
+
+                    typeOfCard == "Visa" -> {
+                        if (currentTransfers * 0.0075 >= 35) {
+                            var comission = currentTransfers * 0.0075
+                            "Комиссия равна $comission руб"
+                        } else {
+                            "Комиссия равна 35 руб"
+                        }
+                    }
+
+                    else -> {
+                        "Комиссия равна 0 руб"
+                    }
+                }
+
+            } else {
+                "В день можно переводить не больше 150 000 руб"
+            }
+
+        } else {
+            var letTransfer = 60000 - previousTransfers
+            if (letTransfer <= 150000) {
+                when {
+                    typeOfCard == "Mastercard" -> {
+                        if (previousTransfers < 75000) {
+                            if (letTransfer + previousTransfers > 75000) {
+                                var comission = (letTransfer - (75000 - previousTransfers)) * 0.006 + 20
+                                "Комиссия равна $comission руб"
+                            } else {
+                                "Комиссия равна 0 руб"
+                            }
+                        } else {
+                            var comission = letTransfer * 0.006 + 20
+                            "Комиссия рвна $comission руб"
+                        }
+                    }
+
+                    typeOfCard == "Visa" -> {
+                        if (letTransfer * 0.0075 >= 35) {
+                            var comission = letTransfer * 0.0075
+                            "Комиссия равна $comission руб"
+                        } else {
+                            "Комиссия равна 35 руб"
+                        }
+                    }
+
+                    else -> {
+                        "Комиссия равна 0 руб"
+                    }
+                }
+
+            } else {
+                "В день можно переводить не больше 150 000 руб"
+            }
+        }
     } else {
         "В месяц можно переводить не больше 600 000 руб"
     }
+
 
 
 
